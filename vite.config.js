331ba9320a95626b8cae98487d27d9152737ae96
Aakaashpadhmanaban <aakaashpadhmanaban@gmail.com>
2026-08-28 17:15:29 +0530

    fix: allow all hosts in Vite dev server to fix Render block

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3001',
